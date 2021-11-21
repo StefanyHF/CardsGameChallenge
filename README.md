1976f36835f865a813a194cb47b39868a94f4345
StefanyHF <stefany.hf9@gmail.com>
2021-11-22 00:45:04 +0100

    refactor

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# CardsGameChallenge
+# Instructions
+
+###1. Build
+
+To build the project, run:
+
+./gradlew build
+
+###2. To execute it:
+
+./gradlew run
+
+###3. To execute the tests:
+
+./gradlew test
--- src/test/java/ShufflerTest.java
+++ src/test/java/ShufflerTest.java
@@ -1,20 +1,46 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
 class ShufflerTest {
 
-    private List<Card> cards = List.of(new Card(1),new Card(2), new Card(3), new Card(4));
+    private final ArrayList<Card> cards = new ArrayList<>(List.of(
+            new Card(1),
+            new Card(2),
+            new Card(3),
+            new Card(4),
+            new Card(5)
+    ));
 
     @Mock
     Random random;
 
-    Shuffler shuffler = new Shuffler(random);
+    Shuffler shuffler;
+
+    @BeforeEach
+    public void setup() {
+        MockitoAnnotations.openMocks(this);
+
+        shuffler = new Shuffler(random);
+    }
 
     @Test
-    public void test() {
+    public void shuffler_should_shuffle_the_list() {
+        Mockito.when(random.nextInt(Mockito.anyInt())).thenReturn(0, 2, 1, 0);
 
+        shuffler.shuffle(cards);
+
+        Assertions.assertEquals(4, cards.get(0).getNumber());
+        Assertions.assertEquals(5, cards.get(1).getNumber());
+        Assertions.assertEquals(2, cards.get(2).getNumber());
+        Assertions.assertEquals(3, cards.get(3).getNumber());
+        Assertions.assertEquals(1, cards.get(4).getNumber());
     }
 }
